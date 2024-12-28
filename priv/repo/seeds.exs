839829aac24e5ac1fd5be757ccb382b513405b1e
Mukul Singh <mukulz2020@gmail.com>
2024-12-28 21:18:34 +0530

    chat app

---
+++ priv/repo/seeds.exs
@@ -0,0 +1,27 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     Chat.Repo.insert!(%Chat.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
+
+posts_path = "priv/repo/seed/posts.json"
+
+alias Chat.Posts
+
+posts_path
+|> File.read!()
+|> Jason.decode!()
+|> Enum.each(fn attrs ->
+  post = %{message: attrs["message"]}
+
+  case Posts.create_post(post) do
+    {:ok, _post} -> :ok
+    {:error, _changeset} -> :error
+  end
+end)
